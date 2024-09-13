ec3bf653972272ff9aa58a24c5e8423365bb6901
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-13 18:56:01 -0300

    feat: instala dependencias do retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.retrofitConverterGson)
+    implementation(libs.retrofit)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
