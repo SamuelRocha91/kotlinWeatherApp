14c095a9328b4ab6d5516a65da9dcdc7d7d840b5
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-13 21:22:06 -0300

    feat: configura retrofit para uma rota get com 3 query parameters

--- app/src/main/java/br/com/trybe/weatherapp/interfaces/OpenWeatherService.kt
+++ app/src/main/java/br/com/trybe/weatherapp/interfaces/OpenWeatherService.kt
@@ -1,9 +1,16 @@
 package br.com.trybe.weatherapp.interfaces
 
+import android.telecom.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface OpenWeatherService {
 
-    @GET
-    fun getCurrentWeatherData()
+    //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    @GET("weather")
+    fun getCurrentWeatherData(
+        @Query("q") cityName: String,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): Call<CurrentWeatherData>
 }
