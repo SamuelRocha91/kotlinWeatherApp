36d94f8cdb6c3671b0f29b3e6de7484bdb117b69
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-14 00:31:28 -0300

    feat: cria um object que instancia um retrofit

---
+++ app/src/main/java/br/com/trybe/weatherapp/interfaces/api/OpenWeather.kt
@@ -0,0 +1,14 @@
+package br.com.trybe.weatherapp.interfaces.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object OpenWeather {
+    val instance: OpenWeatherService by lazy {
+        val retrofit = Retrofit
+            .Builder().baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create()).build();
+
+        retrofit.create(OpenWeatherService::class.java)
+    }
+}
--- app/src/main/java/br/com/trybe/weatherapp/interfaces/api/OpenWeatherService.kt
+++ app/src/main/java/br/com/trybe/weatherapp/interfaces/api/OpenWeatherService.kt
@@ -1,17 +1,15 @@
-package br.com.trybe.weatherapp.interfaces
+package br.com.trybe.weatherapp.interfaces.api
 
-import android.telecom.Call
 import br.com.trybe.weatherapp.data.CurrentWeatherData
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface OpenWeatherService {
 
-    //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     @GET("weather")
     fun getCurrentWeatherData(
         @Query("q") cityName: String,
         @Query("appid") apiKey: String,
         @Query("units") units: String = "metric"
-    ): Call<CurrentWeatherData>
+    ): retrofit2.Call<CurrentWeatherData>
 }
